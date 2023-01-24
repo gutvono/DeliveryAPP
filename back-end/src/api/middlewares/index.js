469c984082cb0a4889046764ab1669a084391e11
Pedro Costa <pedrocostaugusto@gmail.com>
2023-01-24 17:11:42 -0300

    Rota de registro criada e funcional

---
+++ back-end/src/api/middlewares/index.js
@@ -0,0 +1,7 @@
+const validateLoginFields = require('./validateLoginFields');
+const validateRegisterFields = require('./validateRegisterFields');
+
+module.exports= {
+  validateLoginFields,
+  validateRegisterFields
+}
