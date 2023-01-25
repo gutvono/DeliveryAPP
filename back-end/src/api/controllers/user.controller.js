b4005d656d456cbf14bf767604c23eb47f95ab21
Pedro Costa <pedrocostaugusto@gmail.com>
2023-01-25 16:37:03 -0300

    Remove duplicidade de funções 2.0

--- back-end/src/api/controllers/user.controller.js
+++ back-end/src/api/controllers/user.controller.js
@@ -18,15 +18,6 @@ async function userRegister(req, res) {
   res.status(201).json({ response });
 }
 
-async function userRegister(req, res) {
-  const userData = req.body;
-  const { token, error } = await service.userRegister(userData);
-  if (error) {
-    return res.status(error.status).json({ message: error.message });
-  }
-  res.status(201).json({ token });
-}
-
 module.exports = {
   userLogin,
   userRegister,
