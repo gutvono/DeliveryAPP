678c49d0e0031e54a0113fc35c5601d859b715ec
Pedro Costa <pedrocostaugusto@gmail.com>
2023-01-31 19:36:05 -0300

    rota que lista pedidos - Pronta

--- back-end/src/api/controllers/order.controller.js
+++ back-end/src/api/controllers/order.controller.js
@@ -2,13 +2,20 @@ const service = require('../services/order.service');
 
 async function getAOrder(req, res) {
   const { id } = req.params;
-  const order = await service.getAOrder(Number(id));
+  const { order, error } = await service.getAOrder(Number(id));
+  if (error) {
+    return res.status(error.status).json({ message: error.message });
+  }
   res.status(200).json(order);
 }
 
 async function getAllOrders(req, res) {
   const user = req.requestingUser;
-  
+  const { orders, error } = await service.getAllOrders(user);
+  if (error) {
+    return res.status(error.status).json({ message: error.message });
+  }
+  res.status(200).json(orders);
 }
 
 module.exports = {
